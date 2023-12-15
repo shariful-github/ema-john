aa9ca49a3e7f0f6602fe212dfa039e817490b58a
sharif-github <107545661+sharif-github@users.noreply.github.com>
2023-12-15 23:04:48 +0600

    header completed

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import logo from "../assets/images/Logo.svg";
+const Header = () => {
+  return (
+    <div>
+      <div className="bg-[#1C2B35] h-20 flex justify-between items-center">
+          <img className="ml-20" src={logo} alt="" />
+          <div className="text-white mr-20">
+            <a className="ml-7 hover:text-orange-300" href="">Order</a>
+            <a className="ml-7 hover:text-orange-300" href="">Order Review</a>
+            <a className="ml-7 hover:text-orange-300" href="">Manage Inventory</a>
+            <a className="ml-7 hover:text-orange-300" href="">Login</a>
+          </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
